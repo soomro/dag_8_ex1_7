b30c851fedf87bb58835b23fba4809b61a6f9f98
Waseem Akhtar Soomro <ws@waseemsoomro.com>
2016-02-03 11:21:27 +0100

    added problem description. and changed namespace to 'sales'

--- dag_8_ex1_7/Program.cs
+++ dag_8_ex1_7/Program.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace dag_8_ex1_7
+namespace Sales
 {
     class Program
     {
+        /*
+        In this exercise we want to create a class that hold a sale transaction that an employee makes with a client. The sales class should contain a name of the product sold, the money sold for in dollars (double), and a client class that references a client. Also, create a DateTime property that hold the information about when the sale was made. Let the employee class hold a collection of sales objects and implement a method that adds a new sales to the list. Also, create a constructor that instantiates the sales object. The date of the sale can be created in the constructor as the current date (DateTime.Now).
+        */
         static void Main(string[] args)
         {
 
